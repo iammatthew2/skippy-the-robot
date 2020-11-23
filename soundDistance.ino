bc67201e99d6777f1ba68a688ff49e5aebfeff7b
Matthew Villeneuve <mvilleneuve@linkedin.com>
2020-11-23 14:26:42 -0800

    basic servo working

--- soundDistance.ino
+++ soundDistance.ino
@@ -1,13 +1,16 @@
-
+#include <Servo.h>
+Servo servo;
 
 const int trigPin = 11;
 const int echoPin = 12;
 
 long duration;
 int distance;
+int lastDistance = 60;
 void setup() {
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+servo.attach(10);
 Serial.begin(9600); // Starts the serial communication
 }
 void loop() {
@@ -28,7 +31,11 @@ Serial.print(" - tone: ");
 
 Serial.println(map(distance, 0, 50, 2000, 500));
 //tone(14, 500);
-if (distance < 40 ) {
+if (distance < 50 ) {
+  if (abs(distance - lastDistance) > 5) {
+    servo.write(distance * 3); //directs servo to go to position in variable 'angle'
+    lastDistance = distance;
+  }
   tone(14, map(distance, 0, 50, 2000, 500));
   }
 //tone(14, map(distance, 0, 50, 2000, 500));
