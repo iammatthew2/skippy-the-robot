03efab29eed2bfd5a4c9186c70ff562765d983a5
Matthew Villeneuve <mvilleneuve@linkedin.com>
2020-12-31 16:54:29 -0800

    motion tracking added, very scrappy

--- skippy.ino
+++ skippy.ino
@@ -9,7 +9,15 @@ Adafruit_PWMServoDriver pwmServo = Adafruit_PWMServoDriver();
 #define SERVO_MAX 350
 #define SERVO_MIDDLE 270
 #define SERVO_FREQ 50
+#define SERVO_ID_LID_LIFT 4
+#define SERVO_ID_ROTATE 8
 int lidState = SERVO_MIN;  // SERVO_MIN, SERVO_MIDDLE, SERVO_MAX
+#define ROTATION_LEFT_MAX 150
+#define ROTATION_LEFT_PARTIAL 225
+#define ROTATION_RIGHT_MAX 450
+#define ROTATION_RIGHT_PARTIAL 375
+#define ROTATION_MIDDLE 300
+int rotationState = ROTATION_MIDDLE;
 
 // reserve pins
 #define NEO_PIXEL_PIN 21
@@ -34,6 +42,14 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, NEO_PIXEL_PIN, NEO_GRB 
 int waiter = 0;
 int soundInstanceCounter = 0;
 
+#define TRACKING_NOT_TRACKING 0
+#define TRACKING_ACQUIRED 1
+#define TRACKING_LOST 2
+int motionTrackingState = 0;  // 0, 1, 2: not tracking, acquired, lost
+
+#define SEEK_LEFT 0
+#define SEEK_RIGHT 1
+int motionTrackingSeekDirection = 0;  // 0, 1: left, right
 void setup() {
   Serial.begin(9600);
   cute.init(BUZZER_PIN);
@@ -48,21 +64,16 @@ void setup() {
   pwmServo.begin();
   pwmServo.setOscillatorFrequency(27000000);
   pwmServo.setPWMFreq(SERVO_FREQ);
-  servoGoTo(SERVO_MIN);
+  servoGoTo(SERVO_ID_LID_LIFT, SERVO_MIN);
+  servoGoTo(SERVO_ID_ROTATE, ROTATION_MIDDLE);
 
   // neo pixels
   pixels.begin();
   colorWipe(pixels.Color(0, 0, 0), 0);
   cute.play(S_CONNECTION);
-  pwmServo.setPWM(8, 0, 100);
-  delay(1000);
-  pwmServo.setPWM(8, 0, 450);
-  delay(1000);
-  pwmServo.setPWM(8, 0, 500);
 }
 
 void loop() {
-  digitalWrite(BUTTON_LED_PIN, HIGH);
   if (lidState == SERVO_MIN) {
     handleLidDownState();
   }
@@ -84,13 +95,17 @@ void handleLidDownState() {
     cute.playRandom(SG_JOYFUL);
     digitalWrite(BUTTON_LED_PIN, HIGH);
     delay(250);
-
+    if (soundInstanceCounter == 2) {
+      servoGoTo(SERVO_ID_ROTATE, ROTATION_LEFT_PARTIAL, 5);
+      servoGoTo(SERVO_ID_ROTATE, ROTATION_RIGHT_PARTIAL, 5);
+      servoGoTo(SERVO_ID_ROTATE, ROTATION_MIDDLE, 5);
+    }
     if (soundInstanceCounter >= NUM_SOUNDS_UNTIL_OPEN) {
       delay(500);
       soundInstanceCounter = 0;
 
       // open the box mid-way, slowly
-      servoGoTo(SERVO_MIDDLE, 15);
+      servoGoTo(SERVO_ID_LID_LIFT, SERVO_MIDDLE, 15);
       cute.play(S_HAPPY);
       for (int i = 0; i < NUM_PIXELS; i++) {
         pixels.setPixelColor(i, pixels.Color(0, 0, 10));
@@ -103,28 +118,28 @@ void handleLidDownState() {
 void handleLidMiddleState() {
   if (digitalRead(SOUND_SENSOR_PIN) == LOW) {
     cute.playRandom(SG_JOYFUL);
-    servoGoTo(SERVO_MAX, 7);
+    servoGoTo(SERVO_ID_LID_LIFT, SERVO_MAX, 7);
   }
 }
 
 void handleLidUpState() {
-  if (getDistance() < 10) {
+  if (motionTrackingState != TRACKING_NOT_TRACKING) {
+    handleMotionTracking();
+    return;
+  }
+
+  if (getDistance() < 50 && getDistance() > 10) {
     for (int i = 0; i < NUM_PIXELS; i++) {
-      pixels.setPixelColor(i, pixels.Color(10, 0, 0));
+      pixels.setPixelColor(i, pixels.Color(75, 0, 0));
       pixels.show();
     }
+    cute.play(S_OHOOH2);
+    motionTrackingState = TRACKING_ACQUIRED;
+    return;
+  }
 
-    Serial.println("too close - distance sensor deleted less than 10 cm");
-
-    digitalWrite(BUTTON_LED_PIN, HIGH);
-    cute.play(S_DISGRUNTLED);
-    delay(250);
-
-    // close the box
-    servoGoTo(SERVO_MIN);
-
-    delay(1000);
-    colorWipe(pixels.Color(0, 0, 0), 0);
+  if (getDistance() < 10) {
+    closeLid();
   }
 
   for (waiter = 0; waiter <= 1; waiter += 1) {
@@ -141,6 +156,127 @@ void handleLidUpState() {
   }
 }
 
+void closeLid() {
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(10, 0, 0));
+    pixels.show();
+  }
+
+  Serial.println("too close - distance sensor deleted less than 10 cm");
+
+  digitalWrite(BUTTON_LED_PIN, HIGH);
+  cute.play(S_DISGRUNTLED);
+  delay(250);
+
+  // close the box
+  servoGoTo(SERVO_ID_LID_LIFT, SERVO_MIN);
+
+  delay(1000);
+  colorWipe(pixels.Color(0, 0, 0), 0);
+}
+
+void handleMotionTracking() {
+  int targetRotationState;
+
+  if (motionTrackingState == TRACKING_LOST) {
+    // we're seeking left: keep making the numbers smaller
+    if (motionTrackingSeekDirection == SEEK_LEFT) {
+      targetRotationState = rotationState - 1;
+      servoGoTo(SERVO_ID_ROTATE, targetRotationState, 5);
+      if (targetRotationState - 5 < ROTATION_LEFT_MAX) {
+        motionTrackingSeekDirection = SEEK_RIGHT;
+      }
+
+    } else {
+      //we're seeking right
+      targetRotationState = rotationState + 1;
+      servoGoTo(SERVO_ID_ROTATE, targetRotationState, 5);
+      if (targetRotationState + 5 > ROTATION_RIGHT_MAX) {
+        motionTrackingSeekDirection = SEEK_LEFT;
+      }
+    }
+    int distanceCheck = getDistance();
+    if (distanceCheck < 10) {
+      motionTrackingState = TRACKING_NOT_TRACKING;
+      closeLid();
+      return;
+    }
+    if (distanceCheck < 50) {
+      for (int i = 0; i < NUM_PIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(75, 0, 0));
+        pixels.show();
+      }
+      motionTrackingState = TRACKING_ACQUIRED;
+    }
+    return;
+  }
+  if (motionTrackingState == TRACKING_ACQUIRED && getDistance() > 50) {
+    for (int i = 0; i < NUM_PIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(0, 75, 0));
+      pixels.show();
+    }
+    motionTrackingState = TRACKING_LOST;
+  }
+}
+
+void servoGoTo(int servoName, int destination) {
+  servoGoTo(servoName, destination, 0);
+}
+void servoGoTo(int servoName, int destination, int rate) {
+  int currentState = servoName == SERVO_ID_LID_LIFT ? lidState : rotationState;
+  _servoGoFromTo(servoName, currentState, destination, rate);
+}
+
+void _servoGoFromTo(int servoName, int source, int destination, int rate) {
+  if (servoName != SERVO_ID_LID_LIFT && servoName != SERVO_ID_ROTATE) {
+    Serial.print("Error - servoName invalid for _servoGoFromTo: ");
+    Serial.println(servoName);
+    return;
+  }
+
+  if (servoName == SERVO_ID_ROTATE && (destination < ROTATION_LEFT_MAX || destination > ROTATION_RIGHT_MAX)) {
+    Serial.print("destination value out of bounds for rotation: ");
+    Serial.println(destination);
+    return;
+  }
+
+  if (servoName == SERVO_ID_LID_LIFT && (destination < SERVO_MIN || destination > SERVO_MAX)) {
+    Serial.print("destination value out of bounds for lid lift: ");
+    Serial.println(destination);
+    return;
+  }
+
+  if (rate == 0) {
+    pwmServo.setPWM(servoName, 0, destination);
+  } else if (source < destination) {
+    // increment up to a higher servo position
+    for (uint16_t pulselen = source; pulselen < destination; pulselen++) {
+      pwmServo.setPWM(servoName, 0, pulselen);
+      delay(rate);
+    }
+  } else {
+    // decrement down to a lower servo position
+    for (uint16_t pulselen = source; pulselen > destination; pulselen--) {
+      pwmServo.setPWM(servoName, 0, pulselen);
+      delay(rate);
+    }
+  }
+
+  if (servoName == SERVO_ID_LID_LIFT) {
+    lidState = destination;
+  } else {
+    rotationState = destination;
+  }
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+  }
+}
+
 float getDistance() {
   // Clears the DISTANCE_TRIG_PIN
   digitalWrite(DISTANCE_TRIG_PIN, LOW);
@@ -152,38 +288,4 @@ float getDistance() {
 
   // calc distance based on assumed value of speed of sound
   return pulseIn(DISTANCE_ECHO_PIN, HIGH) * 0.034 / 2;
-}
-
-void servoGoTo(int destination) {
-  servoGoTo(destination, 0);
-}
-void servoGoTo(int destination, int rate) {
-  servoGoFromTo(lidState, destination, rate);
-}
-
-void servoGoFromTo(int source, int destination, int rate) {
-  if (rate == 0) {
-    pwmServo.setPWM(4, 0, destination);
-  } else if (source < destination) {
-    // increment up to a higher servo position
-    for (uint16_t pulselen = source; pulselen < destination; pulselen++) {
-      pwmServo.setPWM(4, 0, pulselen);
-      delay(rate);
-    }
-  } else {
-    // decrement down to a lower servo position
-    for (uint16_t pulselen = source; pulselen > destination; pulselen--) {
-      pwmServo.setPWM(4, 0, pulselen);
-      delay(rate);
-    }
-  }
-  lidState = destination;
-}
-
-void colorWipe(uint32_t c, uint8_t wait) {
-  for (uint16_t i = 0; i < pixels.numPixels(); i++) {
-    pixels.setPixelColor(i, c);
-    pixels.show();
-    delay(wait);
-  }
 }
